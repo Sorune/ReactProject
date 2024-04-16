a083329aa27d8ae687723effd5bc7a98b4942094
Sorune <zksktldl125@naver.com>
2024-04-16 17:54:49 +0900

    React Project Read

---
+++ gtt-api-server/src/main/java/com/sorune/gttapiserver/GttApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.sorune.gttapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GttApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GttApiServerApplication.class, args);
+    }
+
+}
---
+++ gtt-api-server/src/test/java/com/sorune/gttapiserver/GttApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorune.gttapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GttApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
