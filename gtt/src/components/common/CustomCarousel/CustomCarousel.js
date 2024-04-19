bc6bd278a144dedda690623466466fd7f4c44c88
Sorune <zksktldl125@naver.com>
2024-04-19 18:04:33 +0900

    CustomCarousel 작성중

--- gtt/src/components/common/CustomCarousel/CustomCarousel.js
+++
@@ -1,7 +0,0 @@
-import {CarouselProps} from "@material-tailwind/react/components/Carousel";
-
-
-export interface CustomCarouselprob extends CarouselProps {
-    deleteButton?: deleteButton;
-
-}
--- gtt/src/test/pages/TestPage.js
+++ gtt/src/test/pages/TestPage.js
@@ -6,7 +6,7 @@ import ContentBody from "../../components/common/ContentBody";
 import UplodComponent from "../../components/common/UplodComponent";
 import LexicalEditor from "../../components/common/LexicalEditor";
 import DropFiles from "../../components/common/DropFiles";
-import CustomCarousel from "../../components/common/CustomCarousel/CustomCarousel";
+import CustomCarousel from "../../components/common/CustomCarousel";
 
 const TestPage = ()=>{
     return (
@@ -25,8 +25,24 @@ const TestPage = ()=>{
             <LexicalEditor />
             <p>Drop Down</p>
             <DropFiles />
-                <p>Custom Carousel</p>
-                <CustomCarousel />
+            <p>Custom Carousel</p>
+                <CustomCarousel>
+                        <img
+                            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                            alt="image 1"
+                            className="h-full w-full object-cover"
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                            alt="image 2"
+                            className="h-full w-full object-cover"
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                            alt="image 3"
+                            className="h-full w-full object-cover"
+                        />
+                </CustomCarousel>
         </BasicLayout>
     )
 }
