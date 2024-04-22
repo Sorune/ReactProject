904edca2c0f3c033cf991f6f27a9f4fe7fda4214
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-04-22 16:38:06 +0900

    경기 예매 페이지 리스트 출력 테스트

--- gtt/src/pages/ticketing/TicketingPage.js
+++ gtt/src/pages/ticketing/TicketingPage.js
@@ -1,39 +1,49 @@
 import { Button, Card, CardBody, Typography } from "@material-tailwind/react";
 
 const TicketingPage = () => {
+    const games = [
+        {
+            matchDay: "5.11(토)",
+            matchHour: "17:00",
+            matchName: "LCK 스프링",
+            matchRound: "1라운드",
+            matchOpponent: "GEN.G vs SKT1"
+        },
+        {
+            matchDay: "5.12(일)",
+            matchHour: "19:00",
+            matchName: "LCK 스프링",
+            matchRound: "2라운드",
+            matchOpponent: "T1 vs KT"
+        }
+    ];
 
     return (
-        <div >
-            <Card className="mt-6 w-full">
-                <CardBody>
-                    <div className="grid col-auto gap-5">
-                        <div className="col-start-1 col-end-1">
-                            <Typography>
-                                {/* {matchDay} */}
-                                5.11(토)
-                            </Typography>
+        <div>
+            {games.map((game, index) => (
+                <Card key={index} className="mt-6 w-full">
+                    <CardBody>
+                        <div className="grid col-auto gap-5">
+                            <div className="col-start-1 col-end-1">
+                                <Typography>{game.matchDay}</Typography>
+                            </div>
+                            <div className="col-start-2 col-end-2">
+                                <Typography>{game.matchHour}</Typography>
+                            </div>
+                            <div className="col-start-3 col-end-3">
+                                <Typography>
+                                    {game.matchName}
+                                    <br/>
+                                    {game.matchRound} / {game.matchOpponent}
+                                </Typography>
+                            </div>
+                            <div className="col-start-4 col-end-4">
+                                <Button className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800">예매</Button>
+                            </div>
                         </div>
-                        <div className="col-start-2 col-end-2">
-                            <Typography>
-                                {/* {matchHour} */}
-                                17:00
-                            </Typography>
-                        </div>
-                        <div className="col-start-3 col-end-3">
-                            <Typography>
-                                {/* {matchName} */}
-                                LCK 스프링
-                                <br/>
-                                {/* {matchRound}/{matchOpponent} */}
-                                1라운드 / GEN.G vs SKT1
-                            </Typography>
-                        </div>
-                        <div className="col-start-4 col-end-4">
-                            <Button className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800">예매</Button>
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
+                    </CardBody>
+                </Card>
+            ))}
         </div>
     );
 }
