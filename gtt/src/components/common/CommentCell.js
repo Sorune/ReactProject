f08742ea377dc56f8b43c31c26d388ad88993623
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-04-22 14:14:43 +0900

    Ticketing 페이지 레이아웃 작업 중.... (상단부분)

--- gtt/src/components/common/CommentCell.js
+++ gtt/src/components/common/CommentCell.js
@@ -11,7 +11,7 @@ const CommentCell = ()=>{
                         <div>
                             <Typography variant="h6">양지웅</Typography>
                             <Typography variant="small" color="gray" className="font-normal">
-                                 Developer
+                                Developer
                             </Typography>
                         </div>
                     </div>
@@ -28,7 +28,7 @@ const CommentCell = ()=>{
                     </Button>
                     {/* 본인일 경우 보이는 수정, 삭제 버튼 */}
                     <Button size="sm" color="blue" variant="text" className="rounded-md">
-               np         modify
+                        modify
                     </Button>
                     <Button size="sm" color="red" variant="text" className="rounded-md">
                         delete
