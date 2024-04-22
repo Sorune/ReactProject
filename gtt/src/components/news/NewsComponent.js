1ded6678d5b9728ac79eb92a97f5d0db810ca092
Sorune <zksktldl125@naver.com>
2024-04-22 14:50:43 +0900

    병합작업중

--- gtt/src/components/news/NewsComponent.js
+++ gtt/src/components/news/NewsComponent.js
@@ -3,7 +3,8 @@ import {Avatar, Typography} from "@material-tailwind/react";
 
 const img = "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg"
 
-const ListComponent = ({serverData}) =>{
+const ListComponent = ({serverData,page,size}) =>{
+    const navigate = useNavigate()
     return (
         <tbody>
             {serverData.dtoList.map( (dto,index) => {
@@ -11,7 +12,7 @@ const ListComponent = ({serverData}) =>{
                 const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
                 return (
-                    <tr key={dto.newsNo}>
+                    <tr key={dto.newsNo} onClick={()=>navigate(`/news/read/${dto.newsNo}?page=1&size=10`)}>
                         <td className={classes}>
                                 <div className="flex items-center gap-3">
                                     <Avatar src={img} alt={dto.theTeam} size="sm"/>
