1a72d706d39856057ed92e2078c06c3a26fa86e5
OZO\ozo <wldnd211@gmail.com>
2024-04-23 11:08:49 +0900

    04-23오전 read페이지

--- gtt/src/pages/news/ReadPage.js
+++ gtt/src/pages/news/ReadPage.js
@@ -15,6 +15,8 @@ import {getComList} from "../../api/commentApi";
 import useCustomMove from "../../hooks/useCustomMove";
 import {useEffect, useState} from "react";
 import CommentCell from "../../components/common/CommentCell";
+import CommentInputCell from "../../components/common/CommentInputCell";
+
 import ContentBody from "../../components/common/ContentBody";
 
 const initState = {
@@ -55,27 +57,29 @@ const ReadPage = () => {
     console.log(comServerData)
     console.log(page, size)
     return (
-        <div className="grid grid-rows-3 grid-cols-9 gap-4">
-            <div className="col-start-3 col-end-8 auto-rows-auto">
+        <div className="grid grid-auto-rows grid-cols-9 gap-4">
+            <div className="col-start-1 col-end-8 row-span-1">
+            <Breadcrumbs fullWidth className="bg-white -z-10">
+                <Link to={'/'} className="opacity-60">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
+                    </svg>
+                </Link>
+                <Link to={'/news'} className="opacity-60">
+                    <span>Components</span>
+                </Link>
+                <a href="#">Breadcrumbs</a>
+            </Breadcrumbs>
+            </div>
+            <div className="col-start-3 col-end-8">
                 <section className="bg-white w-full h-full p-2 py-2">
                     <div className="flex flex-box justify-between items-center">
-                        <Breadcrumbs fullWidth className="bg-white -z-10">
-                            <Link to={'/'} className="opacity-60">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-4 w-4"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
-                                </svg>
-                            </Link>
-                            <Link to={'/news'} className="opacity-60">
-                                <span>Components</span>
-                            </Link>
-                            <a href="#">Breadcrumbs</a>
-                        </Breadcrumbs>
                         <div className="flex p-2">
                             <Button className="rounded-full"
                                     onClick={() => navigate(`/news/list?page=${page}&size=${size}`)}>List</Button>
@@ -106,18 +110,15 @@ const ReadPage = () => {
                                     content....
                                 </Typography>
                             </Card>
-                            <Card className="m-2">
-                                <Textarea label="comment"></Textarea>
-                                <div className="grid grid-cols-8 gap-3 p-2 items-center">
-                                    <Button>Add</Button>
-                                </div>
+                            <Card className="m-2 row-start-3">
+                                <CommentInputCell/>
                             </Card>
                         </CardBody>
                         <CardFooter>
                             <Card className="p-2">
                                 {comServerData.dtoList.map((dto) => {
                                     return (
-                                        <CommentCell key={dto.comNo} writer={dto.writer} content={dto.content}/>
+                                        <CommentInputCell key={dto.comNo} writer={dto.writer} content={dto.content}/>
                                     )
                                 })}
                                 <PageComponent serverData={comServerData} movePage={loadToList} pathName={pathName}/>
