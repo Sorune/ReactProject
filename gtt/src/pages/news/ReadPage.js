e8be56879daecc42ad5f03f99cd7f310755ea468
Sorune <zksktldl125@naver.com>
2024-04-24 12:45:07 +0900

    ReadPage merge and fix

--- gtt/src/pages/news/ReadPage.js
+++ gtt/src/pages/news/ReadPage.js
@@ -21,6 +21,7 @@ import ContentBody from "../../components/common/ContentBody";
 import {useRecoilState} from "recoil";
 import {pageState} from "../../atoms/pageState";
 import {getOne} from "../../api/newsApi";
+import ContentHeader from "../../components/common/ContentHeader";
 
 const initState = {
     dtoList: [],
@@ -84,48 +85,29 @@ const ReadPage = () => {
 
     return (
         <section className="bg-white w-full h-full p-2 py-2">
-            <div className="flex flex-box justify-between items-center">
-                <Breadcrumbs fullWidth className="bg-white -z-10">
-                    <Link to={'/'} className="opacity-60">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
-                        </svg>
-                    </Link>
-                    <Link to={'/news'} className="opacity-60">
-                        <span>Components</span>
-                    </Link>
-                    <a href="#">Breadcrumbs</a>
-                </Breadcrumbs>
-                <div className="flex p-2">
-                    <Button className="rounded-full" onClick={() => moveToList({
-                        pathName: '/news/list',
-                        pageParam: {page: `${page.page}`, size: `${page.size}`}
-                    })}>List</Button>
-                    <Button className="rounded-full">Modify</Button>
-                </div>
-            </div>
+            <ContentHeader page={page} pathName={'/news/list'} moveTo={moveToList} />
             <Card className="flex flex-auto p-1">
                 <CardBody>
-                    <ContentBody ref={ReadQuillRef} teamName={testTeam.teamName} teamImg={testTeam.teamImg} title={serverData.title} content={serverData.content}/>
+                    <ContentBody
+                        ref={ReadQuillRef}
+                        teamName={testTeam.teamName}
+                        teamImg={testTeam.teamImg}
+                        title={serverData.title}
+                        content={serverData.content}
+                        date={serverData.regDate}
+                        viewCount={serverData.hits}
+                    />
                     <Card className="m-2 row-start-3 mt-10">
                         <CommentInputCell/>
                     </Card>
                 </CardBody>
                 <CardFooter>
-                    <Card className="p-2">
-                        {comServerData.dtoList.map((dto) => {
-                            return (
-                                <CommentCell comNo={dto.comNo} writer={dto.writer} content={dto.content}/>
-                            )
-                        })}
-                        <PageComponent serverData={comServerData} movePage={loadToList} pathName={pathName}/>
-                    </Card>
+                    {comServerData.dtoList.map((dto) => {
+                        return (
+                            <CommentCell comNo={dto.comNo} writer={dto.writer} content={dto.content}/>
+                        )
+                    })}
+                    <PageComponent serverData={comServerData} movePage={loadToList} pathName={pathName}/>
                 </CardFooter>
             </Card>
         </section>
