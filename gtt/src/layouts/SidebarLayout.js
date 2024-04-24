65765c736866b67e2af95c86e04a929ff6e0c6f9
OZO\ozo <wldnd211@gmail.com>
2024-04-24 11:34:09 +0900

    ReadPage 수정

--- gtt/src/layouts/SidebarLayout.js
+++ gtt/src/layouts/SidebarLayout.js
@@ -22,13 +22,13 @@ const SidebarLayout = ({ children }) => {
             {/* 기존 헤더 대신 BasicMenu */}
             <BasicMenu />
             {/* 상단 여백 my-5 제거 */}
-            <div className="bg-white w-full flex flex-col px-8 gap-5 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+            <div className="bg-white w-full flex flex-col px-8 gap-2 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
                 {/* 첫 번째와 다섯 번째 열은 isMobile이 true일 때 hidden 클래스 추가 */}
                 <div className={`col-span-1 p-2 ${isMobile ? "hidden" : ""}`}>
                     <Sidebar />
                 </div>
                 {/* 두 번째와 네 번째 열은 isMobile이 true일 때 hidden 클래스 추가 */}
-                <main className="bg-sky-300 w-full px-5 py-5 md:col-span-2 m-0">
+                <main className="bg-sky-300 w-full  md:col-span-2 m-0">
                     {children}
                 </main>
                 {/* 세 번째와 여섯 번째 열은 isMobile이 true일 때 hidden 클래스 추가 */}
--- gtt/src/test/pages/Sidebar.js
+++ gtt/src/test/pages/Sidebar.js
@@ -10,7 +10,7 @@ import {
 
 const Sidebar = ()=>{
     return(
-        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+        <Card className="max-h-[calc(100vh-1rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
             <div className="mb-2 p-4">
                 <Typography variant="h5" color="blue-gray">
                     Sidebar
