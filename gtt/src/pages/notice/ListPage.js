9c27c7b0bc5dd33c03f8360ff9466ab8c825ea71
OZO\ozo <wldnd211@gmail.com>
2024-04-26 10:04:02 +0900

    공지사항 게시판 시작 페이지만 만든 상태

---
+++ gtt/src/pages/notice/ListPage.js
@@ -0,0 +1,13 @@
+import NoticeLayout from "../../layouts/NoticeLayout";
+import BasicLayout from "../../layouts/BasicLayout";
+
+const ListPage = () => {
+    return(
+           <div className="p-4 w-full bg-white">
+               <div className="text-3xl font-extrabold">
+                   List Page
+               </div>
+           </div>
+    )
+}
+export default ListPage;
