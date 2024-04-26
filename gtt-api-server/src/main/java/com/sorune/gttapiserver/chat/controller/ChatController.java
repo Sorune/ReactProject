53c824c888a3beee6146926494d2cf45450bfde7
Sorune <76832007+Sorune@users.noreply.github.com>
2024-04-26 22:27:59 +0900

    어노테이션 수정, 자동 주입이 되지 않는 문제 해결

--- gtt-api-server/src/main/java/com/sorune/gttapiserver/chat/controller/ChatController.java
+++ gtt-api-server/src/main/java/com/sorune/gttapiserver/chat/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.sorune.gttapiserver.chat.controller;
 
 import com.sorune.gttapiserver.chat.DTO.ChatMessageDTO;
-import com.sorune.gttapiserver.chat.DTO.ChatRoomDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -13,12 +12,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @MessageMapping("/api/chat/message")
     public void message(ChatMessageDTO chatMessageDTO) {
         if(ChatMessageDTO.MessageType.JOIN.equals(chatMessageDTO.getMessageType()))
             chatMessageDTO.setMessage(chatMessageDTO.getSenderId()+"님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/api/chat/message"+chatMessageDTO.getChatRoomId(), chatMessageDTO);
+        simpMessageSendingOperations.convertAndSend("/sub/api/chat/message"+chatMessageDTO.getChatRoomId(), chatMessageDTO);
     }
 }
