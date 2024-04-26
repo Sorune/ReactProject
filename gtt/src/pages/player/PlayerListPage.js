92838090de9b55f5138c2858c74c60bfccc3d598
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-04-26 10:54:33 +0900

    충돌해결

---
+++ gtt/src/pages/player/PlayerListPage.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+=======
+import ListComponent from "../../components/player/PlayerListComponent"
+
+const PlayerListPage = () => {
+
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">
+                Player List Page
+            </div>
+            <ListComponent/>
+        </div>
+    )
+}
+
+export default PlayerListPage;
+>>>>>>> 83c4374 (player repository, service 리스트 불러올 때 오름차순으로 불러오도록 변경,)
