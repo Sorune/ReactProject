bf1f6b14a80b63abe40cd00631d4844b845b7b77
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-04-26 10:34:44 +0900

    변경사항 없음

--- gtt/src/api/memberApi.js
+++ gtt/src/api/memberApi.js
@@ -6,21 +6,35 @@ const API_SERVER_HOST = 'http://localhost:8001';
 
 // 회원 리스트 출력
 export const memberList = async () => {
-    const respons = await axios.get(`${API_SERVER_HOST}/list`);
+    const res = await axios.get(`${API_SERVER_HOST}/list`);
     return respons.data;
 };
 
+// 회원 한명 조회
+export const getMember = async () => {
+    const res = '';
+}
+
 // 회원 삭제
 export const removeMember = async (num) => {
-    const respons = await axios.delete(`${API_SERVER_HOST}/${num}`);
-    return respons.data;
+    const res = await axios.delete(`${API_SERVER_HOST}/${num}`);
+    return res.data;
 }
 
 // 회원수정
 export const updateMember = async (userId, pw, nick, birth, zoneCode, adress, addrSub) => {
-    const respons = await axios.put(`${API_SERVER_HOST}/${num}`,
+    const res = await axios.put(`${API_SERVER_HOST}/${num}`,
         {
-            
-        }
-        )
+            "userId":userId,
+            "pw":pw,
+            "nick":nick,
+            "birth":birth,
+            "zoneCode":zoneCode,
+            "adress":adress,
+            "addrSub":addrSub
+        })
+        .then(function (res)  {
+
+        })
+        .catch();
 }
