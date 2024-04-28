abbb3b0d07ba75991b143cc7d5dc7007beb8eec5
OZO\ozo <wldnd211@gmail.com>
2024-04-28 22:11:02 +0900

    add완료 및 수정, 삭제 진행중 (모달 및 리스트 돌아가는 로직 , read처리 문제 있음)  설명 듣고 처리할 예정

---
+++ gtt/src/components/notice/NoticeAddComponent.js
@@ -0,0 +1,100 @@
+import {useState} from "react";
+import {Button} from "@material-tailwind/react";
+import {postAdd} from "../../api/noticeApi";
+import ResultModal from "../common/ResultModal";
+import useCustomMove from "../../hooks/useCustomMove";
+import {useSearchParams} from "react-router-dom";
+
+// 초기활를 담당
+const initState = {
+    title:'',
+    writer:'',
+    content:'',
+    regDate:''
+}
+const NoticeAddComponent = () =>{
+    const [queryparams] = useSearchParams()
+    const page = queryparams.get("page") ? parseInt(queryparams.get("page")) : 1
+    const size = queryparams.get("size") ? parseInt(queryparams.get("size")) : 10
+
+    const [notice, setNotice] = useState(initState)
+
+    // 결과데이터가 있는 경우 modal을 보여준다.
+    const [result, setResult] = useState(null) // 결과상태
+
+    const handleChangeNotice = (e)=> {
+        notice[e.target.name] = e.target.value
+        setResult(result.notiNo)
+        setNotice({...notice})
+    }
+
+    const handleClickAdd = () => {
+        //console.log(notice)
+        postAdd(notice).then(result => {
+            console.log(result)
+            //초기화
+            setNotice(initState)
+        }).catch(e => {
+            console.error(e)
+        })
+    }
+const {moveToList} = useCustomMove()
+    const closeModal = () =>{
+        setResult(null)
+        //moveToList({'list' {page:page,size:size}})
+    }
+
+    return(
+        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+
+            {/*모달 처리*/}
+            {result ? <ResultModal title={'공지사항'} content={`새로운 게시물${result} 번 게시물이 추가되었습니다.`}
+                callbackFn={closeModal}/>: <></> }
+
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">TITLE</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+                           name="title"
+                           type={'text'}
+                           value={notice.title}
+                           onChange={handleChangeNotice} >
+                    </input>
+
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">WRITER</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+                           name="writer"
+                           type={'text'}
+                           value={notice.writer}
+                           onChange={handleChangeNotice}
+                           >
+                    </input>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">CONTENT</div>
+                    <textarea className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+                              name="content"
+                              value={notice.content}
+                              onChange={handleChangeNotice}
+                              >
+                    </textarea>
+                </div>
+            </div>
+
+            <div className="flex justify-end">
+                <div className="relative mb-4 flex p-4 flex-wrap items-stretch">
+                    <Button onClick={handleClickAdd}>ADD</Button>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default NoticeAddComponent;
