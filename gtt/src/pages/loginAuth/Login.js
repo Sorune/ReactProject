e1f8f3d70054c9ffa82ddfe50431685cc2faf65a
jeon pilu <pirwoos@naver.com>
2024-04-30 09:34:19 +0900

    ... 시큐리티 오류

---
+++ gtt/src/pages/loginAuth/Login.js
@@ -0,0 +1,107 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useId, usePw } from "../../hooks/useUserAuth";
+import {login} from "../../api/loginApi";
+
+
+
+
+
+const Login = () => {
+    // useLogin에서 리턴한 값/메서드
+    const [loginID, insertIdChange] = useId("");
+    const [loginPW, insertPwChange] = usePw("");
+
+    const navigate = useNavigate();
+    // 회원가입 페이지로 이동하는 메서드
+    const moveToLink = (e) => {
+        // 클릭한 요소에서 'data-value' 속성 값을 가져와서 목적지 변수에 저장
+        const destination = e.target.dataset.value;
+        // 목적지 값에 따라 다르게 처리 "login"이면 로그인 페이지로 이동하고
+        switch (destination) {
+            case "/" :          // 홈메인으로 이동
+                navigate("/");
+                break;
+            case "signIn" :     // 회원가입으로 이동
+                navigate("/signIn");
+                break;
+        }
+    };
+
+    const goLogin = async ({userId}) => {
+        e.preventDefault();
+        try {
+            // const data = await handleLogin(loginID, loginPW);
+            const data = await login(loginID, loginPW);
+            console.log(data);
+            if(data === true) {
+                alert('로그인 성공 : ' + data.message);
+                navigate('/');  // 성공 후 메인으로 이동
+            }else if(data === false) {
+                alert('로그인 실패 : ' + data.message);
+            }
+            // switch (data) {
+            //     case "SUCCESS" :
+            //         alert('로그인 성공 : ' + data.message);
+            //         return navigate('/');  // 성공 후 메인으로 이동
+            //     case "FAILURE" :
+            //         alert('로그인 실패 : ' + data.message);
+            //         break;
+            // }
+
+        } catch (error) {
+            alert(error.message);  // API의 오류 메시지 또는 처리된 오류 표시
+        }
+    }
+
+
+
+    return (
+        <div>
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" data-value="/" onClick={moveToLink} className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        {/* <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"> */}
+                        GTT    
+                    </a>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                LOGIN
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" onSubmit={goLogin}>
+                                <div>
+                                    <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID</label>
+                                    <input type="text" value={loginID} onChange={insertIdChange} name="id" id="id" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your id"/>
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" value={loginPW} onChange={insertPwChange} name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••"/>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">로그인정보 기억하기</label>
+                                        </div>
+                                    </div>
+                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">ID/PW 찾기</a>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                    로그인
+                                </button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    계정이 없으신가요? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500" data-value="signIn" onClick={moveToLink}>회원가입</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Login;
---
+++ gtt/src/pages/loginAuth/SignIn.js
@@ -0,0 +1,221 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import AddrWithDaum from './AddrWithDaum';
+import DatePicker from "../../components/common/DatePicker";
+import { Collapse, Button, IconButton, Card, Typography, CardBody } from '@material-tailwind/react';
+import { Dialog, DialogHeader, DialogBody } from '@material-tailwind/react';
+
+// validation.js 제외 / 리액트 훅 규칙에 따라서 사용할 훅의 네임을 useInput으로 새로운 훅을 생성
+import {
+    useConfirmID,
+    useConfirmNick,
+    usePasswordMatch,
+    useBirth,
+    useAddrSub
+} from '../../hooks/useInput';
+
+const SignIn = () => {
+    const {join,validateID} = joinApi();
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+    const [address, setAddress] = useState('');
+    const [zoneCode, setZoneCode] = useState('');
+    const [modalOpen, modalSetOpen]  = React.useState(false);
+    const [termsChecked, setTermsChecked] = useState(false);
+    // useInput에서 리턴한 값
+    const [inputID, inputIdChange, idValid, checkID, clearInput] = useConfirmID('');
+    const [inputNick, inputNickChange, nickMatch, clearNick] = useConfirmNick('');
+    const [inputPass, inputPassChange, inputConfirmPass, inputConfirmPassChange, passMatch, clearPassword] = usePasswordMatch("", "");
+    const [birth, insertBirthChange] = useBirth('');
+    const [addrSub, insertAddrSubChange] = useAddrSub('');
+
+    // useInput에서 리턴한 값을 기준으로 메서드 실행 - id검증
+    /*
+    *
+    * */
+    const idCompareResult = () => {
+        if(idValid === false) {
+            alert("이미 사용중인 아이디 입니다.");
+            clearInput();
+        }else {
+            alert("사용 가능한 아이디 입니다.");
+        }
+    };
+
+    // 리턴한 값을 기준으로 메서드 실행 - 닉네임검증
+    const nickCompareResult = () => {
+        if(nickMatch) {
+            alert("이미 사용중인 닉네임 입니다.");
+            clearNick();
+        }
+    }
+    // 리턴한 값을 기준으로 메서드 실행 - 비밀번호검증
+    const passCompareResult = () => {
+        if(!passMatch) {
+            alert("PW 입력 과 PW 입력확인이 일치하지 않습니다.");
+            clearPassword();
+        }
+    }
+    // 머트리얼 콜랩스 동작메서드 - toggle
+    const toggleOpen = () => setOpen((cur) => !cur);
+    // 머트리얼 모달 동작 메서드
+    const modalHandleOpen = () => modalSetOpen(true);
+    const modalHandleClose = () => modalSetOpen(false);
+    // 주소 검색 후 결과 값을 반환하는 메서드
+    const handleUpdateAddress = (fullAddress, zoneCode) => {
+        setAddress(fullAddress);
+        setZoneCode(zoneCode);
+    };
+    // 로그인 페이지로 가는 메서드 - 버튼 이벤트 처리용
+    const moveToLink = (e) => {
+        // 클릭한 요소에서 'data-value' 속성 값을 가져와서 목적지 변수에 저장
+        const destination = e.target.dataset.value;
+        // 목적지 값에 따라 다르게 처리 "login"이면 로그인 페이지로 이동하고
+        if (destination === "login") {
+            navigate("/login");
+            // "home"이면 홈페이지(보통 "/"로 표시됨)로 이동
+        } else if (destination === "home") {
+            navigate("/");
+        }
+    };
+
+    // 폼 제출 메서드
+    const joinMember = async (e) => {
+        // 제출 동작 막기
+        e.preventDefault();
+        if (!termsChecked) {
+            alert("개인정보 취급방침에 동의해야 합니다.");
+
+        } else {
+            // 로그인 api 메서드 호출
+            const data = await join(inputID, inputPass, inputNick, birth, zoneCode, address, addrSub);
+            alert("가입여부 : " + data);
+            // 경고창 출력 후 홈으로 이동
+            // navigate("/login");
+        }
+    }
+
+    return (
+        <div>
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        GTT
+                    </a>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                JOIN
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={joinMember}>
+                                <div>
+                                    <label htmlFor="userId" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID 입력</label>
+                                    <input name="userId" value={inputID} onChange={inputIdChange} onBlur={checkID} type="text" id="userId" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="아이디를 입력하세요" />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">PW 입력</label>
+                                    {/* PW */}
+                                    <input name="password" value={inputPass} onChange={inputPassChange} type="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                                </div>
+                                <div>
+                                    <label htmlFor="password2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">PW 입력 확인</label>
+                                    {/* confirmPW */}
+                                    <input name="password2" value={inputConfirmPass} onChange={inputConfirmPassChange} onBlur={passCompareResult} type="password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                                </div>
+                                <div>
+                                    <label htmlFor="nickname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">닉네임</label>
+                                    {/* nick */}
+                                    <input name="nickname" value={inputNick} onChange={inputNickChange} onBlur={nickCompareResult} type="text" id="nick" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="닉네임을 입력하세요"/>
+                                </div>
+
+                                <div>
+                                    <Button onClick = { toggleOpen }>
+                                        추가정보작성
+                                    </Button>
+                                    <Collapse open = {open}>
+                                        <Card className="my-4 mx-auto w-12/12">
+                                            <CardBody>
+                                                <div>
+                                                    <Typography as="label" htmlFor="birth" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">생년월일</Typography>
+                                                    {/*<input type="date" value={birth} onChange={insertBirthChange} name="birth" required/>*/}
+                                                    {/*<DatePicker value={birth} onChange={insertBirthChange} name="birth"/>*/}
+
+                                                    {/* birth */}
+                                                    <input type={"date"} value={birth} onChange={insertBirthChange} name="birth"/>
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="zoneCode" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">우편번호</label>
+                                                    <div className="grid grid-cols-12 gap-5">
+                                                        <div className='col-span-10'>
+                                                            {/* zoneCode */}
+                                                            <input type="text" value={zoneCode || ''} name="zoneCode" onChange={setZoneCode} id="zoneCode" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" readOnly />
+                                                        </div>
+                                                        <div className='col-span-2'>
+                                                            <IconButton onClick = {modalHandleOpen} variant="gradient" title='주소검색'>
+                                                                <svg className="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                                    <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                                                                </svg>
+                                                            </IconButton>
+                                                            <Dialog open={modalOpen} handler={modalHandleClose}>
+                                                                <DialogHeader>
+                                                                    <div className="grid grid-cols-12 gap-5 w-full">
+                                                                        <div className='col-span-10 text-center'>
+                                                                            주소찾기
+                                                                        </div>
+                                                                        <div className='col-span-2'>
+                                                                            <Button color='red' onClick={modalHandleClose} variant="gradient" title='창닫기'>
+                                                                                X
+                                                                            </Button>
+                                                                        </div>
+                                                                    </div>
+                                                                </DialogHeader>
+                                                                <DialogBody>
+                                                                    <AddrWithDaum onClose={modalHandleClose} onUpdateAddress={handleUpdateAddress} />
+                                                                </DialogBody>
+                                                            </Dialog>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="adress" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">주소</label>
+                                                    {/* address */}
+                                                    <input type="text" name="address" id="address" onChange={setAddress} value={address || ''} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="주소입력" readOnly />
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="addrSub" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">나머지 주소</label>
+                                                    {/* addrSub */}
+                                                    <input type="text" name="addrSub" id="addrSub" onChange={insertAddrSubChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="나머지 주소입력"/>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                    </Collapse>
+                                </div>
+                                {/* 체크박스 */}
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input checked={termsChecked} onChange={(e) => setTermsChecked(e.target.checked)} id="terms" type="checkbox" />
+                                    </div>
+                                    <label htmlFor="terms" className="ml-3 text-sm font-light text-gray-500 dark:text-gray-300">
+                                        개인정보 취급방침에 동의합니다
+                                    </label>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                    회원가입
+                                </button>
+                                <p className='ml-3 text-sm font-light text-gray-500 dark:text-gray-300'>
+                                    이미 계정이 있으신가요? &nbsp;
+                                    <a href="#none" className="text-primary" data-value="login" onClick={moveToLink}>로그인</a>
+                                    <br />
+                                    메인화면으로 돌아갈까요? &nbsp;
+                                    <a href="#none" className="text-primary" data-value="home" onClick={moveToLink}>홈으로</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default SignIn;
