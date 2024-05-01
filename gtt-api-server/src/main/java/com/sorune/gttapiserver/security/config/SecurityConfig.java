5aa07a83b75ae62e8371cfcbc9354b4fe852640f
Sorune <zksktldl125@naver.com>
2024-05-01 13:47:37 +0900

    Security Config 수정 -- 인증되지 않은 유저 접근권한 문제 -> AuthenticationEntryPoint 등록

--- gtt-api-server/src/main/java/com/sorune/gttapiserver/security/config/SecurityConfig.java
+++ gtt-api-server/src/main/java/com/sorune/gttapiserver/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.sorune.gttapiserver.security.filter.JWTCheckFilter;
 import com.sorune.gttapiserver.security.handler.APILoginFailHandler;
 import com.sorune.gttapiserver.security.handler.APILoginSuccessHandler;
 import com.sorune.gttapiserver.security.handler.CustomAccessDeniedHandler;
+import com.sorune.gttapiserver.security.handler.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
@@ -70,7 +71,8 @@ public class SecurityConfig {
                 .logout(httpSecurityLogoutConfigurer ->
                         httpSecurityLogoutConfigurer.logoutUrl("/api/member/logout"))
                 .exceptionHandling(config->
-                        config.accessDeniedHandler(new CustomAccessDeniedHandler()))
+                        config.accessDeniedHandler(new CustomAccessDeniedHandler())
+                                .authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
         ;
 
 
