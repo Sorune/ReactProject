4a4cf3df0734b6382e5324b685b09da66c587cbe
Sorune <76832007+Sorune@users.noreply.github.com>
2024-05-03 01:23:42 +0900

    카카오 로그인 컴포넌트 체크

--- gtt/src/components/user/KakaoLoginComponent.js
+++ gtt/src/components/user/KakaoLoginComponent.js
@@ -9,10 +9,10 @@ const KakaoLoginComponent = ()=>{
         <div className={"flex flex-col"}>
             <div className={"text-center text-blue-500"}>로그인시에 자동 가입처리 됩니다</div>
             <div className={"flex justify-center w-full"}>
-                <div className={"text-3xl text-center m-6 text-white font-extrabold w-3/4 bg-yellow-500 shadow-sm rounded p-2"}>
                     <Link to={link}><img src={"/img/kakao_login_medium_narrow.png"}/></Link>
-                </div>
             </div>
         </div>
     )
 }
+
+export default KakaoLoginComponent
