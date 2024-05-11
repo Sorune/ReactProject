8974646630daebd57ec7f86fac016ba5375af7d5
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-05-11 10:50:18 +0900

    이미지 추가

--- gtt/src/pages/ticketing/TicketingPage.js
+++ gtt/src/pages/ticketing/TicketingPage.js
@@ -79,26 +79,86 @@ const TicketingPage = ({ selectedTeam }) => {
 
     const totalPages = Math.ceil(filteredGames.length / gamesPerPage);
 
+    const handleOpenModal = useCallback((game) => {
+        setCurrentGame(game);
+        setModalOpen(true);
+    }, [setCurrentGame, setModalOpen]);
+
     const changePage = (newPage) => {
         if (newPage < 1 || newPage > totalPages) return;
         setCurrentPage(newPage);
     };
+    const [showDialog, setShowDialog] = useState(false);
 
+    const handleOpenDialog = () => {
+        setShowDialog(true)
+    };
+    const handleCloseDialog = () => {
+        setShowDialog(false);
+    };
     return (
         <div>
+            {/* Game cards */}
             {paginatedGames.map((game, index) => (
-                <GameCard key={index} game={game} selectedTeam={selectedTeam} />
+                <GameCard key={index} game={game} onOpenModal={handleOpenModal} />
             ))}
+            {/* Pagination */}
             <div className="flex justify-center space-x-2 mt-4">
                 <Button onClick={() => changePage(currentPage - 1)} disabled={currentPage === 1}>이전</Button>
                 {Array.from({ length: totalPages }, (_, index) => (
-                    <Button key={index} onClick={() => setCurrentPage(index + 1)} className={index + 1 === currentPage ? 'bg-blue-700' : ''}>
+                    <Button key={index} onClick={() => setCurrentPage(index + 1)}
+                            className={index + 1 === currentPage ? 'bg-blue-700' : ''}>
                         {index + 1}
                     </Button>
                 ))}
                 <Button onClick={() => changePage(currentPage + 1)} disabled={currentPage === totalPages}>다음</Button>
             </div>
+            {/* Modal for booking details */}
+            <Dialog open={modalOpen} onClose={() => setModalOpen(false)} handler={handleOpenModal}>
+                <Dialog.Header>예매 정보</Dialog.Header>
+                <Dialog.Body>
+                    {currentGame && (
+                        <div>
+                            <p>경기일자: {currentGame.matchDay}</p>
+                            <p>주최명: {currentGame.matchName}</p>
+                            <p>상대팀: {currentGame.matchOpponent}</p>
+                            <p>경기정보: {currentGame.matchRound}</p>
+                        </div>
+                    )}
+                </Dialog.Body>
+                <Dialog.Footer>
+                    <Button onClick={() => {
+                        handleOpenDialog(currentGame);
+                        setModalOpen(false);
+                    } } >예매</Button>
+                    <Button onClick={() => setModalOpen(false)}>닫기</Button>
+                </Dialog.Footer>
+            </Dialog>
+            {/*좌석선택*/}
+            <Dialog size="lg" className="w-full" open={showDialog} handler={handleCloseDialog} >
+                <DialogHeader className="flex justify-between bg-gradient-to-r from-cyan-500 to-blue-500 bg-gradient-to-bl items-center">
+                    <div className="flex items-center">
+                        <Typography color="white" variant="h3" className="mr-4">
+                            예매하기
+                        </Typography>
+                        {currentGame && (
+                            <Typography className="mr-4" color="white" variant="h4">
+                                {currentGame.matchName} | {currentGame.matchRound}
+                            </Typography>
+                        )}
+                    </div>
+                    <Button variant="gradient" color="red" size="sm" onClick={handleCloseDialog}>
+                        X
+                    </Button>
+                </DialogHeader>
+                <DialogBody className="overflow-hidden ">
+                    <div className="max-w-full max-h-full h-[34.5rem]">
+                        <TestStadium/>
+                    </div>
+                </DialogBody>
+            </Dialog>
         </div>
+
     );
 }
 
