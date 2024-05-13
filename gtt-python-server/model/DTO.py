76d7bd86987a7bd23c364f7cf473ef74e0dda574
Sorune <zksktldl125@naver.com>
2024-05-13 09:25:50 +0900

    변경사항 확인

--- gtt-python-server/model/DTO.py
+++ gtt-python-server/model/DTO.py
@@ -1,13 +1,38 @@
-class Player:
-    def __init__(self, nick_name, name, name_full, country, age, birth_date, roles, fav_champs):
-        self.nick_name = nick_name
-        self.name = name
-        self.name_full = name_full
-        self.country = country
-        self.age = age
-        self.birth_date = birth_date
-        self.roles = roles
-        self.fav_champs = fav_champs
+from typing import List, Dict, Optional
+from sqlalchemy import String,Integer,Float,Boolean,DateTime,Date
+from sqlalchemy.orm import Mapped,mapped_column,relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# class Player:
+#     def __init__(self, nick_name, name, name_full, country, age, birth_date, roles, fav_champs):
+#         self.nick_name = nick_name
+#         self.name = name
+#         self.name_full = name_full
+#         self.country = country
+#         self.age = age
+#         self.birth_date = birth_date
+#         self.roles = roles
+#         self.fav_champs = fav_champs
+#
+#     def __str__(self):
+#         return f'{self.nick_name}/{self.name}/{self.name_full}/{self.country}/{self.age}/{self.birth_date}/{self.roles}/{self.fav_champs}'
+#
+class Player(Base):
+    __tablename__ = 'server_player'
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    nick_name:Mapped[str] = mapped_column(String(255))
+    name:Mapped[str] = mapped_column(String(255))
+    name_full:Mapped[str] = mapped_column(String(255))
+    country:Mapped[str] = mapped_column(String(255))
+    age:Mapped[int]  =  mapped_column(Integer)
+    birth_date:Mapped[str] = mapped_column(Date)
+    roles:Mapped[List["roles"]] = relationship(back_populates="roles")
+    fav_champs:Mapped[List["fav_champs"]] = relationship(back_populates="fav_champs")
+    def __repr__(self) -> str:
+        return f"Player(id={self.id!r}, nick_name={self.nick_name!r}, name={self.name!r}, name_full={self.name_full}, country={self.country!r}, age={self.age!r}, birth_date={self.birth_date!r}"
 
     def __str__(self):
         return f'{self.nick_name}/{self.name}/{self.name_full}/{self.country}/{self.age}/{self.birth_date}/{self.roles}/{self.fav_champs}'
