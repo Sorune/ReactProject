515d7a6d1f4f1c9469978230d6065b506f986796
LAPTOP-B699FSDS\lhd03 <lhd0317@hanmail.net>
2024-05-16 18:22:22 +0900

    api 경로 수정 8001 삭제 후 8080으로 변경

--- gtt/src/api/filesApi.js
+++ gtt/src/api/filesApi.js
@@ -3,8 +3,7 @@ import axios from "axios";
 //export const SERVER = 'http://sorune.asuscomm.com'
 //export const SERVER = 'http://mbc-webcloud.iptime.org'
 export const SERVER = 'http://localhost'
-//export const API_SERVER_HOST = `${SERVER}:8080`
-export const API_SERVER_HOST = `${SERVER}:8001`
+export const API_SERVER_HOST = `${SERVER}:8080`
 
 export const SERVER_HOST = `${SERVER}:3000`
 const prefix = `${API_SERVER_HOST}/api/files`;
